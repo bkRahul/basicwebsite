91d25eefa9ee1a40f5ede5983391530404b5e009
bkRahul <rahulsatya1068@gmail.com>
2019-04-20 22:59:22 +0530

    added routes

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', 'PagesController@home' );
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/about', 'PagesController@about' );
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::get('/contact', 'PagesController@contact' );
 
 Route::post('/contact/submit', 'MessagesController@submit' );
+
+Route::get('/messages', 'MessagesController@showMessages' );
