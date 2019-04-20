5e9f0c576e5763de6003b969427e100dbf696cdb
bkRahul <rahulsatya1068@gmail.com>
2019-04-20 22:58:59 +0530

    added blade template files

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,4 +2,25 @@
 
 @section('content')
 <h1>Contact Us</h1>
+
+<form method="POST" action="/contact/submit">
+{{ csrf_field() }}
+    <div class="form-group">
+      <label for="validationTooltip01">Name</label>
+      <input type="text" name="name" class="form-control" id="validationTooltip01" placeholder="Username" required>
+    </div>    
+    <div class="form-group">
+        <label for="FormControlInput1">Email address</label>
+        <input type="email" name="email" class="form-control" id="FormControlInput1" placeholder="name@example.com">
+    </div>
+    <div class="form-group">
+        <label for="FormControlTextarea1">Message</label>
+        <textarea name="message" class="form-control" id="FormControlTextarea1" rows="3"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+    @if(session('success'))
+       <div class="alert alert-success">{{ session('success') }}</div>
+    @endif  
+</form>
+
 @endsection
--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,13 +1,14 @@
 <nav class="navbar-expand-lg navbar navbar-dark bg-primary">
-<a class="navbar-brand" href="#">BasicWebsite</a>
+<a class="navbar-brand" href="/">BasicWebsite</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="/">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="/about">About Us</a>
-      <a class="nav-item nav-link disabled" href="/contact">Contact Us</a>
+      <a class="nav-item nav-link {{ Request::is('/') ? 'active' : '' }}" href="/">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link {{ Request::is('about') ? 'active' : '' }}" href="/about">About Us</a>
+      <a class="nav-item nav-link {{ Request::is('contact') ? 'active' : '' }}" href="/contact">Contact Us</a>
+      <a class="nav-item nav-link {{ Request::is('messages') ? 'active' : '' }}" href="/messages">Messages</a>
     </div>
   </div>
 </nav>
